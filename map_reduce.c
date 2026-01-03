d59a9ad898cc207aa370599dec4d9399a744c42f
Nagavignesh1729 <mnaga.1729@gmail.com>
2026-01-03 10:17:43 -0800

    implementation and testing map funciton

--- map_reduce.c
+++ map_reduce.c
@@ -8,13 +8,31 @@
 #include <stdlib.h>
 #include <pthread.h>
 
-void* map(void* arg) {
-    printf("This is a function\n");
-    return NULL;
-}
+struct map_args {
+    int start;
+    int end;
+    char* buffer;
+};
 
-void* print_thread(void* arg) {
+// arg -> address of integer pointer -> (start, end) 
+void* map(void* arg) {
+    struct map_args* input = (struct map_args *)arg;
+    int start = input->start;
+    int end = input->end;
+    char* buffer = input->buffer;
+
     int freq[256] = {0};
+    int i;
+
+    for(i=start; i<end; i++) {
+        freq[buffer[i]]++;
+    }
+
+    for(i=0; i<256; i++) {
+        if (freq[i] != 0)
+            printf("%c - %d\n", i, freq[i]);
+    }
+
     return NULL;
 }
 
@@ -22,6 +40,18 @@ int main() {
     FILE* f;
     char* buffer;
     long size;
+    struct map_args* chunks[4];
+    pthread_t t1, t2;
+
+    for(int i=0; i<2; i++) {
+        chunks[i] = (struct map_args *)malloc(sizeof(struct map_args));
+    }
+    chunks[0]->start = 0;
+    chunks[0]->end = 30;
+
+    chunks[1]->start = 30;
+    chunks[1]->end = 80;
+    
 
     f = fopen("test.txt", "rb");
     
@@ -36,7 +66,18 @@ int main() {
 
     buffer = (char *)malloc(size + 1);
     fread(buffer, 1, size, f);
-    buffer[size] = "\0";
+    buffer[size] = '\0';
+
+    chunks[0]->buffer = buffer;
+    chunks[1]->buffer = buffer;
+
+    printf("Size of test file: %ld\n", size);
+
+    pthread_create(&t1, NULL, map, (void *)chunks[0]);
+    pthread_create(&t2, NULL, map, (void *)chunks[1]);
+
+    pthread_join(t1, NULL);
+    pthread_join(t2, NULL);
 
     fclose(f);
 
